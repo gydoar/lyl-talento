21175fbae65a6c88e0fac11d96dce62a5d0824f1
Andres Vega <gydoar@hotmail.com>
2015-02-24 11:10:38 -0500

    Agregando Framework Option themes

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+
+define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/inc/' );
+require_once dirname( __FILE__ ) . '/inc/options-framework.php';
 //Soporte para menu
 	register_nav_menus(array(
 		'menu-top'=>'Menu header',
