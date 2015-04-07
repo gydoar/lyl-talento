430324d9c12165962068a419db7c8d23c711ab15
Andres Vega <gydoar@hotmail.com>
2015-04-07 17:54:21 -0500

    agregando el archivo footer home

---
+++ footer-home.php
@@ -0,0 +1,10 @@
+<!-- Footer contacto -->
+
+	<div class="blue-fuerte">
+		<section class="container contacto">
+			<h3>CONTACTO</h3>
+			<a href="mailto:direjecutiva@lyltalento.com">direjecutiva@lyltalento.com</a>
+			<p><strong>Tel:</strong> (+57) 1 776 3293 - <strong>Cel:</strong> 300 256 4420</p>
+			<p>Cra 80B No 56 - 65 | Bogotá - Colombia</p>
+		</section>
+	</div>
